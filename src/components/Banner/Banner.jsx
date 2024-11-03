9c86ece0f05cac7fbe1dcff1e58913152eb60ea8
jeff397 <jeffdelmotte@gmail.com>
2024-11-03 14:32:35 +0100

    intégration de la banniére

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,6 +5,8 @@ function banner() {
     return (
     <div className='banner'>
         <img src={homeBanner} alt="bannière" />
+        <div className="banner_overlay"></div>
+        <h1 className='banner_title'>Chez vous, partout et ailleurs</h1>
     </div>
     );
 }
