3754bd8211b08bb42d42e589543cd937cd3029ce
jeff397 <jeffdelmotte@gmail.com>
2024-11-03 12:42:57 +0100

    integration du header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,9 +8,9 @@ function Header() {
       <div className='header_logo'>
       <img src={homeLogo} alt="Logo" />
       </div>
-      <div className='header_link'>
-        <Link to="/">Accueil</Link>
-        <Link to="/about">A propos</Link>
+      <div className='header_links'>
+        <Link to="/" className='header_link'>Accueil</Link>
+        <Link to="/about" className='header_link'>A propos</Link>
       </div>
     </nav>
   );
