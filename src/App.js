825bba536dd3a44282a5016d625d8f646a5f44e8
jeff397 <jeffdelmotte@gmail.com>
2024-11-05 11:42:50 +0100

    application du css de base sur toute les pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./components/Base/_base.scss";
+import Base from "./components/Base/Base";
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Footer from './components/Footer/Footer';
@@ -8,11 +10,12 @@ import Footer from './components/Footer/Footer';
 function App() {
   return (
     <Router>
+      <Base>       
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        
+        <Route path="/about" element={<About />} />        
       </Routes>
+      </Base>
       <Footer />
     </Router>
   );
---
+++ src/components/Base/Base.jsx
@@ -0,0 +1,10 @@
+// src/components/Base/Base.jsx
+import React from 'react';
+
+const Base = ({ children }) => (
+  <div className="base">
+    {children}
+  </div>
+);
+
+export default Base;
