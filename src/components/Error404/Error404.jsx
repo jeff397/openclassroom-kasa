c57b9042f2f6545e07d4fb14ddcc2c83eeb20b79
jeff397 <jeffdelmotte@gmail.com>
2024-11-18 21:00:02 +0100

    mode responsive

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import "./error404.scss";
+import errorImage from "../../assets/images/404.png";
 
 function Error404() {
   return (
     <main>
       <div className="error">
-        <span className="error-number">404</span>
+        <img className="error-number" src={errorImage} alt="error-404" />
         <span className="error-text">
-          Oups! La page que vous demandez n'existe pas
+          Oups! La page que <br className="break" /> vous demandez n'existe pas
         </span>
         <Link to="/" className="error-link">
           Retourner sur la page d'accueil
